0323feccb71e9888b897e8ef4dc4c9d9f7125a8f
Zheyang Song <jerome_soung@hotmail.com>
2023-01-26 23:11:07 -0800

    improve readme

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import { Scheduler } from './scheduler';
-import { TWorkerMaker } from './types';
+import { Scheduler } from "./scheduler";
+import { TWorkerMaker } from "./types";
 
-export { REQ_EARLY_TERMINATION_TOKEN } from './constants';
-export { TWorkerMaker } from './types';
+export { REQ_EARLY_TERMINATION_TOKEN } from "./constants";
+export { TWorkerMaker } from "./types";
 
 type SchedulerInstance<I, O> = InstanceType<typeof Scheduler<I, O>>;
 type ISwarmed<I, O> = SchedulerInstance<I, O>["doRequest"] & {
@@ -13,13 +13,26 @@ type ISwarmedWithResourceSaving<I, O> = ISwarmed<I, O> & {
   enableRecycling: () => void;
 };
 
-export function swarm<I, O>(workerMaker: TWorkerMaker, maxCount: number, recyclable?: true): ISwarmedWithResourceSaving<I, O>;
-export function swarm<I, O>(workerMaker: TWorkerMaker, maxCount: number, recyclable: false): ISwarmed<I, O>;
-export function swarm<I = any, O = any>(workerMaker: TWorkerMaker, maxCount: number, recyclable = true) {
+export function swarm<I, O>(
+  workerMaker: TWorkerMaker,
+  args: { maxCount?: number; recyclable?: true }
+): ISwarmedWithResourceSaving<I, O>;
+export function swarm<I, O>(
+  workerMaker: TWorkerMaker,
+  args: { maxCount?: number; recyclable: false }
+): ISwarmed<I, O>;
+export function swarm<I = any, O = any>(
+  workerMaker: TWorkerMaker,
+  {
+    maxCount = 3,
+    recyclable = true,
+  }: { maxCount?: number; recyclable?: boolean } = {}
+) {
   const scheduler = new Scheduler<I, O>(workerMaker, maxCount, recyclable);
 
   if (recyclable) {
-    const swarmed: ISwarmedWithResourceSaving<I, O> = (req) => scheduler.doRequest(req);
+    const swarmed: ISwarmedWithResourceSaving<I, O> = (req) =>
+      scheduler.doRequest(req);
     swarmed["disableRecycling"] = () => scheduler.pauseResourceSaving();
     swarmed["enableRecycling"] = () => scheduler.restartResourceSaving();
     swarmed["terminate"] = () => scheduler.kill();
