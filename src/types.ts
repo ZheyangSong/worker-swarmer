55731ab296a6fe45899bbd5b2af124b1c00a8059
Zheyang Song <jerome_soung@hotmail.com>
2023-02-15 11:00:51 -0800

    add support to transferable

--- src/types.ts
+++ src/types.ts
@@ -4,6 +4,7 @@ export type TInterruptableReq<T> = T | typeof REQ_EARLY_TERMINATION_TOKEN;
 
 export interface IQueueRequest<I, O> {
   details: I;
+  transferred?: Transferable[];
   report: (data: TInterruptableReq<O>) => void;
 }
 
